04ca6dcaba413a72ee1da1c6830224b8728005cc
FeketeMiklosBenjamin <fekete.miklos.benjamin@students.jedlik.eu>
2021-05-09 17:58:45 +0200

    A téglalap területe

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ def main() -> None:
         téglalap_kerület: float = 2 * (a + b)
         print(f'A téglalap kerülete: {téglalap_kerület}')
 
+    print('A téglalap területét kiszámoló függvény!')
+    a : float = float(input('a (oldal) = '))
+    b : float = float(input('b (oldal) = '))
+    if a < 1 or b < 1:
+        print('A megadott adatokkal nem lehet számolni!')
+    else:
+        téglalap_terület: float = a * b
+        print(f'A téglalap területe: {téglalap_terület}')
+
 
 if __name__ == "__main__":
     main()
